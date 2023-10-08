600747821a73799bd91c972a02f889cf85caf781
sivasurya489 <vemulassurya@gmail.com>
2023-10-08 11:26:40 -0700

    Update division.c

--- division.c
+++ division.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main()
+ {
   int a,b,c;
     printf("Enter any Number\n");
     scanf("%d", &a);
 
     printf("Enter another Number\n");
     scanf("%d", &b);
-c = a / b;
+    if (b == 0)
+    {
+      printf("division by zero is impossible\n");
+      return 0;
+    }  
+    c = a / b;
     printf("Division of 2 numbers: %d", c);
     return 0;
 }
